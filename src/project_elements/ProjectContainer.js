732b451e88e86e6a4b28848aad70738202fae415
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-08-25 21:17:36 -0400

    Project container now accepts variable number of items

--- src/project_elements/ProjectContainer.js
+++ src/project_elements/ProjectContainer.js
@@ -3,23 +3,17 @@ import Project from './Project.js';
 import image from '../logo192.png';
 
 function ProjectContainer(props) {
+	const projects = props.items.map((item) => 
+		<Project projectName={item.title} 
+		imageLocation={item.imageLocation}
+		linkLocation={item.linkDestination}
+		altText={item.altText}
+		/>
+	);
+	
 	return (
 		<div class="row">
-			<Project projectName={props.items[0].title} 
-			imageLocation={props.items[0].imageLocation}
-			linkLocation={props.items[0].linkDestination}
-			altText={props.items[0].altText}
-			/>
-			<Project projectName={props.items[1].title} 
-			imageLocation={props.items[1].imageLocation}
-			linkLocation={props.items[1].linkDestination}
-			altText={props.items[1].altText}
-			/>
-			<Project projectName={props.items[2].title} 
-			imageLocation={props.items[2].imageLocation}
-			linkLocation={props.items[2].linkDestination}
-			altText={props.items[2].altText}
-			/>
+			{projects}
 		</div>
 	);
 }

92b59dc70da0835c02fbb9110b49aeee49434817
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-09-01 17:56:56 -0400

    Added real projects

---
+++ data_classes/project_objects.js
@@ -0,0 +1,21 @@
+export const projectsList = [
+	{
+		title: "Game of Life",
+		imageLocation: '',
+		linkDestination: 'https://github.com/thisisjacob/GameOfLife',
+		altText: 'Pixels representing the game of life spread throughout a screen'},
+	{
+		title: 'Hack Assembler',
+		imageLocation: '',
+		linkDestination: 'https://github.com/thisisjacob/HackAssembler',
+		altText: 'Assembler' 
+	},
+	{
+	title: 'This Website!',
+	imageLocation: '',
+	linkDestination: 'https://github.com/thisisjacob/my_website',
+	altText: 'Image of this Website'
+	}
+]
+
+

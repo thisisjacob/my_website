143b26792464391e7449de95e9c3938050cd7f02
jacob h <42303925+thisisjacob@users.noreply.github.com>
2020-09-02 22:11:47 -0400

    Further apperance improvements

--- src/project_elements/ProjectPageSection.js
+++ src/project_elements/ProjectPageSection.js
@@ -4,18 +4,18 @@ import ProjectContainer from './ProjectContainer.js';
 
 function ProjectPageSection(props) {
 	return (
-		<div name="project section" class="border border-dark rounded">
-			<div name="project introduction">
+		<div name="project section" class="border border-dark rounded p-4">
+			<div name="project introduction" class="p-4">
 				<h2>Projects</h2>
 				<p>All of my programming projects!</p>
 			</div>
-			<div name="featured projects" class="border border-primary pb-5">
+			<div name="featured projects" class="border border-primary p-4">
 				<h3>Featured Projects</h3>
 				<p>Some of my special projects.</p>
 				<ProjectContainer/>
 			</div>
 			
-			<div name="other projects" class="border border-primary rounded-bottom">
+			<div name="other projects" class="border border-primary p-4">
 				<h3>Other Projects</h3>
 				<div name="other projects list" class="list-group">
 					<a href='#' class="list-group-item list-group-item-action">Testing1</a>
